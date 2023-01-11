1dc42244046fd9b31a8e60808b1bd6783f6a15a1
zhulishen <zhulishen0331@gmail.com>
2023-01-12 00:59:30 +0900

    add: banner page

--- src/pages/HomePage/Banner/index.tsx
+++ src/pages/HomePage/Banner/index.tsx
@@ -4,7 +4,18 @@ import './style.scss';
 
 const Banner = () => {
     return (
-        <div></div>
+        <div className="banner">
+            <div className="banner-item">
+                <a href="https://www.greengrower.club" target="_blank" rel="noopener noreferrer">
+                    <img src="https://goatminers.com/sugcxas/Green-Grower/Green-Grower-top.gif" alt="" title="" width="320" height="100" style={{ borderRadius: 5 }} />
+                </a>
+            </div>
+            <div className="banner-item">
+                <a href="https://infinitybeans.app" target="_blank" rel="noopener noreferrer">
+                    <img src="https://goatminers.com/sugcxas/Infinity-Beans/Infinity-Beans-top.gif" alt="" title="" width="320" height="100" style={{ borderRadius: 5 }} />
+                </a>
+            </div>
+        </div>
     );
 }
 
