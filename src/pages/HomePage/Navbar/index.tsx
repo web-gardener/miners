0fcbef6697bbeb2bf2862aece722b1f6cfcc2994
zhulishen <zhulishen0331@gmail.com>
2023-01-11 23:19:37 +0900

    add: navbar/collapse menu

--- src/pages/HomePage/Navbar/index.tsx
+++ src/pages/HomePage/Navbar/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Marquee from 'react-fast-marquee';
 import { FaBars } from 'react-icons/fa'
+import { IoMdInformationCircle } from 'react-icons/io';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+
 import LogoImg from '../../../assets/image/logo.png';
 import './style.scss';
 
 const Navbar = () => {
+    const [collapseMenu, setCollapseMenu] = useState(true);
     return (
         <div className='navbar'>
             <div className='logo'>
@@ -31,7 +36,37 @@ const Navbar = () => {
                 </div>
             </div>
             <div className='menu-btn'>
-                <FaBars />
+                <FaBars onClick={() => setCollapseMenu(!collapseMenu)} />
+                {!collapseMenu &&
+                    <div className="dropdown-menu">
+                        <ul>
+                            <li>
+                                <div className='icon'>
+                                    <IoMdInformationCircle />
+                                </div>
+                                <div className='title'>
+                                    <p>Learn More</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='icon'>
+                                    <FaTelegramPlane />
+                                </div>
+                                <div className='title'>
+                                    <p>Telegram</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='icon'>
+                                    <BsTwitter />
+                                </div>
+                                <div className='title'>
+                                    <p>Twitter</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                }
             </div>
         </div >
     );
