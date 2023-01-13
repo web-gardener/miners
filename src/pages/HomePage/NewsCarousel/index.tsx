67c4efec3ac51948bfb2898ac776341bbb99753d
zhulishen <zhulishen0331@gmail.com>
2023-01-14 01:05:23 +0900

    implement: get data at news carousel using api

--- src/pages/HomePage/NewsCarousel/index.tsx
+++ src/pages/HomePage/NewsCarousel/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Carousel from 'react-material-ui-carousel'
 import { AiFillStar } from 'react-icons/ai'
+import axios from 'axios';
 import './style.scss';
 
 const NewsCarousel = () => {
-    var items = [
+    const [items, setItems] = useState([
         {
             heading: "Recently Added",
             list: [
@@ -37,8 +38,50 @@ const NewsCarousel = () => {
                 { title: "Backed Beans 2", content: "+31.53%" },
             ]
         },
+    ]);
 
-    ]
+    useEffect(() => {
+        axios
+            .get("https://api.goatminers.com/json_data_top_gainers")
+            .then(response => {
+                let data = response.data;
+                setItems([
+                    {
+                        heading: "Recently Added",
+                        list: [
+                            { title: data[6].nameRecently, content: "$" + data[6].tvlUsdRecently },
+                            { title: data[7].nameRecently, content: "$" + data[7].tvlUsdRecently },
+                            { title: data[8].nameRecently, content: "$" + data[8].tvlUsdRecently },
+                        ]
+                    },
+                    {
+                        heading: "Top TVL",
+                        list: [
+                            { title: data[9].nameTopTvl, content: "$" + data[9].tvlUsdTopTvl },
+                            { title: data[10].nameTopTvl, content: "$" + data[10].tvlUsdTopTvl },
+                            { title: data[11].nameTopTvl, content: "$" + data[11].tvlUsdTopTvl },
+                        ]
+                    },
+                    {
+                        heading: "Top 24h",
+                        list: [
+                            { title: data[3].nameTvl24h, content: "$" + data[3].tvl24h },
+                            { title: data[4].nameTvl24h, content: "$" + data[4].tvl24h },
+                            { title: data[5].nameTvl24h, content: "$" + data[5].tvl24h },
+                        ]
+                    },
+                    {
+                        heading: "Top 7 days",
+                        list: [
+                            { title: data[0].nameTvl7d, content: "$" + data[0].tvl7d },
+                            { title: data[1].nameTvl7d, content: "$" + data[1].tvl7d },
+                            { title: data[2].nameTvl7d, content: "$" + data[2].tvl7d },
+                        ]
+                    },
+                ])
+            })
+            .catch(err => { console.log(err) })
+    }, [])
 
     return (
         // <Carousel>
